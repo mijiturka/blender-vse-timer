10af677b3bd3f35116a3f67a81019c7b6c71f096
Rumyana <mijiturka@tuta.io>
2021-07-19 21:43:22 +0100

    Clean up timer.py
    Strip duplication functionality is still incomplete

--- timer.py
+++ timer.py
@@ -21,19 +21,6 @@ class AnimatedTimer(bpy.types.Operator):
         print(strip)
         strip.text = "New text now!"
 
-        strip_new = strip.copy()
-        # for i in range(10):
-#             scene.sequence_editor.link(strip_new)
-#
-#             strip_new.location = (obj.location * factor) + (cursor * (1.0 - factor))
-#
-#         bpy.ops.sequencer.duplicate_move(
-#             SEQUENCER_OT_duplicate={"mode":'TRANSLATION'},
-#             TRANSFORM_OT_seq_slide={"value":(0, 0), "snap":False,
-# "snap_target":'CLOSEST', "snap_point":(0, 0, 0), "snap_align":False,
-# "snap_normal":(0, 0, 0), "release_confirm":False}
-#         )
-
         return {'FINISHED'}
 
 def menu_func(self, context):
